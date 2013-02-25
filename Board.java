97c74f86751c399864dd8537f2b8c6e80c9ba63e
Naomi Plasterer <nplaster@mymail.mines.edu>
2013-02-24 20:33:54 -0700

    added skelton of board.java

--- Board.java
+++ Board.java
@@ -1,4 +1,27 @@
 //Naomi and Brandon
 public class Board {
+	private ArrayList<BoardCell> cells = new ArrayList<BoardCell>();
+	private Map<Character, String> rooms = new Map<Character, String>();
+	private int numRows;
+	private int numColumns;
+	
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void loadConfigFiles(){
+		//add logic and more helper methods
+	}
 
+	public void calcIndex(int row, int column){
+		//add logic
+	}
+	
+	public BoardCell GetRoomCellAt(int row, int column){
+		//add logic
+		return cells;
+	}
+	
+	//need to add getters for the board fields
+	
 }
