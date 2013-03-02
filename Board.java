570f34556cb39d5c887df876fc180b4c2a64497d
Naomi Plasterer <nplaster@mymail.mines.edu>
2013-03-02 16:58:52 -0700

    copied over methods and variables from IntBoard

--- Board.java
+++ Board.java
@@ -2,6 +2,7 @@ package clueGame;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -23,6 +24,7 @@ public class Board {
 	//Defaults to Dr. Rader's config files
 	private String legend;
 	private String board;
+	private boolean[] visited;
 	
 	public Board() {
 		cells = new ArrayList<BoardCell>();
@@ -154,11 +156,46 @@ public class Board {
 	}
 	
 	public void calcTargets(int location, int steps) {
-		
+		LinkedList<Integer> adjs = new LinkedList<Integer>();
+		for(Integer i : adjMtx.get(location)) {
+			if(!visited[i])
+				adjs.add(i);
+		}
+		for(Integer adjCell : adjs) {
+			visited[adjCell] = true;
+			if(steps == 1) 
+				targets.add(adjCell);
+			else
+				calcTargets(adjCell, steps - 1);
+			visited[adjCell] = false;
+		}
 	}
 	
 	public void calcAdjacencies(){
-		
+		LinkedList<Integer> adjs;
+		for(int row = 0; row <= 3; row++) {
+			for(int column = 0; column <= 3; column++) {
+				adjs = new LinkedList<Integer>();
+				if(row == 3) 
+					adjs.add(calcIndex(row-1,column));
+				else if(row == 0)
+					adjs.add(calcIndex(row+1,column));
+				else {
+					adjs.add(calcIndex(row+1,column));
+					adjs.add(calcIndex(row-1,column));
+				}
+
+				if(column == 3)
+					adjs.add(calcIndex(row,column-1));
+				else if(column == 0)
+					adjs.add(calcIndex(row,column+1));
+				else {
+					adjs.add(calcIndex(row,column+1));
+					adjs.add(calcIndex(row,column-1));
+				}
+				adjMtx.put(calcIndex(row,column),adjs);
+			}
+		}
 	}
 	
 	public HashSet getTargets(){
@@ -170,7 +207,12 @@ public class Board {
 	}
 	
 	public void startTargets(int location, int steps){
-		
+		//empty targets and set visited to false just in case
+		targets = new HashSet<Integer>();
+		Arrays.fill(visited, false);
+		//set start location to true
+		visited[location] = true;
+		calcTargets(location,steps);
 	}
 	
 }
